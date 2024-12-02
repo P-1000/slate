e090e452ec817861c2dacdee7c30900d50d07f03
pavanpatchikarla <76902617+P-1000@users.noreply.github.com>
2024-12-02 19:43:37 +0530

    Update preload.ts

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,24 +1,25 @@
 import { ipcRenderer, contextBridge } from 'electron'
 
-// --------- Expose some API to the Renderer process ---------
 contextBridge.exposeInMainWorld('ipcRenderer', {
   on(...args: Parameters<typeof ipcRenderer.on>) {
-    const [channel, listener] = args
-    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args))
+    const [channel, listener] = args;
+    return ipcRenderer.on(channel, (event, ...args) => listener(event, ...args));
   },
   off(...args: Parameters<typeof ipcRenderer.off>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.off(channel, ...omit)
+    const [channel, ...omit] = args;
+    return ipcRenderer.off(channel, ...omit);
   },
   send(...args: Parameters<typeof ipcRenderer.send>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.send(channel, ...omit)
+    const [channel, ...omit] = args;
+    return ipcRenderer.send(channel, ...omit);
   },
   invoke(...args: Parameters<typeof ipcRenderer.invoke>) {
-    const [channel, ...omit] = args
-    return ipcRenderer.invoke(channel, ...omit)
+    const [channel, ...omit] = args;
+    return ipcRenderer.invoke(channel, ...omit);
   },
-
-  // You can expose other APTs you need here.
-  // ...
-})
+  getClipboardData: () => ipcRenderer.invoke('get-clipboard-data'),
+  deleteClipboardData: (id: string) => ipcRenderer.invoke('delete-clipboard-item', id),
+  pinClipboardData: (id: string) => ipcRenderer.invoke('pin-clipboard-item', id),
+  fetchLinkPreview: (url :any) => ipcRenderer.invoke('get-link-preview', url),
+  copyToClipboard: (data :any) => ipcRenderer.invoke('copy-to-clipboard', data),
+});
